dc5796f66136684085d07a67ef7b593d5a8c163e
Hector Robles <hjt.robles@gmail.com>
2024-11-26 12:33:06 -0500

    update ecosystemconfig

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,9 @@
-// path: .ecosystem.config.js"
 module.exports = {
   apps: [
     {
       name: 'strapi',
       script: 'npm',
       args: 'start',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production',
       },
