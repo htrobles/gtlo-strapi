3a51766ae45ae78b0ba8f1d49fa45e5645893677
Hector Robles <hjt.robles@gmail.com>
2024-11-27 02:42:28 -0500

    include email plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,21 @@
-export default () => ({});
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        secure: true,
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: 'no-reply@gospeltrendlondon.org',
+        defaultReplyTo: 'connect@gospeltrendlondon.org',
+      },
+    },
+  },
+});
